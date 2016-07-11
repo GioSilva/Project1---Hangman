432d905f6263e7be0bf95e403928e8991a4b202a
GIOMAR SILVA <gsilva23@gmail.com>
2016-07-11 15:54:15 -0400

    Splitting words and adding new header

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 $(document).on("ready", function() {
 console.log("Hangmanned")
-var words = ['html', 'javascript', 'jquery', 'laptop', 'statement'];
-function chooseWord () {
-    return words[Math.floor(Math.random() * words.length)];
-  }
-console.log(chooseWord())
+var words = ["html", "javascript", "jquery", "laptop", "statement", "pizza"];
+var userChoices = [];
+
+// function randomWord() {
+//     return words[Math.floor(Math.random() * words.length)];
+//   }
+// console.log(randomWord())
+
+var random = words[Math.floor(Math.random() * words.length)];
+ console.log(random);
+
+var splitWord = random.split("");
+
+console.log(splitWord)
+
+
+
+
 })
