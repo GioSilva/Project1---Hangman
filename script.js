f00c309b48acf47a53c7563b28989ee88c2b4a0b
GIOMAR SILVA <gsilva23@gmail.com>
2016-07-12 11:20:56 -0400

    Fixed input box, matching user input with random word

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 $(document).on("ready", function() {
-console.log("Hangmanned")
-var words = ["html", "javascript", "jquery", "laptop", "statement", "pizza"];
-var userChoices = [];
+  console.log("Hangmanned")
+  var words = ["html", "javascript", "jquery", "laptop", "statement", "pizza", "argument", "linux", "gui"];
+  var userChoices = [];
 
-// function randomWord() {
-//     return words[Math.floor(Math.random() * words.length)];
-//   }
-// console.log(randomWord())
-
-var random = words[Math.floor(Math.random() * words.length)];
- console.log(random);
-
-var splitWord = random.split("");
-
-console.log(splitWord)
+  // Selecting a random word from 'words' array:
+  var random = words[Math.floor(Math.random() * words.length)];
+   console.log(random);
 
 
+  // Preventing default
+  $(".button").on("click", function(evt){
+      evt.preventDefault();
 
 
+    // Splitting random word into letters and creating new array:
+    var splitWord = random.split("");
+    console.log(splitWord)
+
+    // Getting user input
+    var newInput = $("#guess > .input").val()
+    console.log(newInput);
+
+    // Adding user input to userChoices array
+    userChoices.push(newInput);
+    console.log(userChoices)
+
+    // Checking if user letter matches letter in random word
+    console.log( random.includes(newInput));
+
+    console.log()
+        $("#guess > .input").val("")
+  })
 })
