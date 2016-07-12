eeed740e010e808868b0b6db0246acd976ba57d9
GIOMAR SILVA <gsilva23@gmail.com>
2016-07-12 13:26:24 -0400

    Adding underscores, random word and matching working correctly

--- script.js
+++ script.js
@@ -6,16 +6,25 @@ $(document).on("ready", function() {
   // Selecting a random word from 'words' array:
   var random = words[Math.floor(Math.random() * words.length)];
    console.log(random);
+   // Splitting random word into letters and creating new array:
+  var splitWord = random.split("");
+   console.log(splitWord)
 
+  var wordLength = splitWord.length;
+
+  var underscores = "";
+
+  for(i=0; i<wordLength; i++) {
+    underscores = underscores + "_ "
+    }
+    console.log(underscores)
+    // $("body").append("<div>_</div>");
+
+    $(".spaces").html(underscores);
 
   // Preventing default
   $(".button").on("click", function(evt){
       evt.preventDefault();
-
-
-    // Splitting random word into letters and creating new array:
-    var splitWord = random.split("");
-    console.log(splitWord)
 
     // Getting user input
     var newInput = $("#guess > .input").val()
@@ -26,9 +35,21 @@ $(document).on("ready", function() {
     console.log(userChoices)
 
     // Checking if user letter matches letter in random word
-    console.log( random.includes(newInput));
+    var TrueOrFalse = splitWord.includes(newInput);
+
+    // Console.log just to make sure everything's working:
+    console.log(TrueOrFalse);
 
     console.log()
         $("#guess > .input").val("")
+
+
+    // if (TrueOrFalse == "True") {
+    //   console.log("Good guess!")
+    // } else {
+    //   console.log("1 step closer to death")
+    // }
+
+
   })
 })
